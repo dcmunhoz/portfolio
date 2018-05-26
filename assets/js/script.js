ffe533889f6a6139725bc316376586ee69b7d5b5
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-05-26 19:26:43 -0300

    Adicionada animação na nav, alterado background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
+// Variaveis globais
+var navAtivada = 0;
+
 $(document).ready(function(){
     // Alterar a navbar conforme o scroll
-
-
-
-
-
-
-
+    changeNav();
 
 });
+
+function changeNav(){
+    $(window).scroll(function(){
+
+        //Pega a posição da NAVBAR
+        var posY = $('nav').offset().top;
+        
+        //console.log(posY);
+        
+        // Se estiver de acordo com as condições, altera o formato da navbar, caso contrario volta ao normal.
+        if(posY >= 75 && navAtivada == 0){
+            $('nav').addClass('nav-active');
+            navAtivada = 1;
+        }else if(posY < 75 && navAtivada == 1){
+            $('nav').removeClass('nav-active');
+            navAtivada = 0;
+        }
+    });
+}
