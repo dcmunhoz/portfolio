f1da6136ce06946259d40f37e29a3ffe5a4df744
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-05-26 20:36:09 -0300

    Adicionado Parallax no background

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
+//
+// @author      : Daniel Costa Munhoz 
+// @deacription : Script que comporta todas as animações, entre outros pontos visuais do portifolio
+// 
+
 // Variaveis globais
 var navAtivada = 0;
 
+// Funções chamadas fora do READY pois é necessário ser executada tambem quando a pagina der reload
+changeNav();
+backgroundScroll();
+
 $(document).ready(function(){
     // Alterar a navbar conforme o scroll
-    changeNav();
+    
 
 });
 
+// Alterar navbar de acordo com o scroll da pagina
 function changeNav(){
     $(window).scroll(function(){
 
@@ -23,5 +33,23 @@ function changeNav(){
             $('nav').removeClass('nav-active');
             navAtivada = 0;
         }
+    });
+}
+
+// Efeito parallax no background quando o scroll na pagina é efetuado
+function backgroundScroll(){
+    $(window).scroll(function(){
+
+        // Captura a posição da pagina
+        var backgroundPosition = $(window).scrollTop();
+        var blur = backgroundPosition / 300;
+        
+        console.log(blur);
+
+        //Seta a propriedade background position y do background negativa(para subir), multiplicando o valor do scroll. Qaunto maior o valor da multiplicação, mais rapido sera o scroll
+        $('#background').css({'background-position-y': -(backgroundPosition * .5)});
+
+        //Aplica um blur na imagem conforme o scroll
+        $('#background').css({"filter": "blur("+blur+"px)" });
     });
 }
